8cbce5e4b3ff19dcb079d475d06bcf2d3f88f426
Gabriel Frontório <gabriel.frontorio@IFRSL.redelocal>
2019-09-12 10:58:51 -0300

    []

--- controller/Controller.php
+++ controller/Controller.php
@@ -6,10 +6,7 @@ require "autoload.php";
 class Controller{
 
     public function pesquisa($url){
-        $json = file_get_contents($url);
-        $jsonDecode = (json_decode($json, true));
-
-        var_dump($jsonDecode);
+        
     }
 
 }
